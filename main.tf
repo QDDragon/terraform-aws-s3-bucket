29ca08fbb1a0768c753ec13c1f10013030e1f377
QDDragon <37509674+QDDragon@users.noreply.github.com>
2023-09-01 08:13:47 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket" "this" {
 
   force_destroy       = var.force_destroy
   object_lock_enabled = var.object_lock_enabled
-  tags                = var.tags
+  tags                = "v1.0.4"
 }
 
 resource "aws_s3_bucket_logging" "this" {
